82971dd98c055916ccfff8955515277a39b3a586
lucidbeaming <zenphoto@gmail.com>
2017-06-08 00:55:52 -0700

    setup

---
+++ ZynAddSubFX/required-packages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get --no-install-recommends install jackd2
+sudo apt-get -y install a2jmidid
+sudo apt-get -y install laditools
+sudo apt-get -y install libfltk1.3 libfltk-images1.3 liblo7 libmxml1
+sudo apt-get -y install autoconf
+sudo apt-get -y install libtool
+sudo apt-get -y install git
+sudo apt-get -y install cmake
+sudo apt-get -y install premake
+sudo apt-get -y install cmake-curses-gui
+sudo apt-get -y install fftw3-dev
+sudo apt-get -y install libmxml-dev
+sudo apt-get -y install zlib1g-dev # installed by default
+sudo apt-get -y install libasound2-dev
+sudo apt-get -y install libjack-jackd2-dev
+sudo apt-get -y install libfltk1.3-dev
+sudo apt-get -y install non-ntk-dev
+sudo apt-get -y install libncurses-dev
+sudo apt-get -y install liblo-dev
+sudo apt-get -y install dssi-dev
+sudo apt-get -y install libjpeg-dev
+sudo apt-get -y install libxpm-dev
+sudo apt-get -y install liblash-compat-dev
+sudo apt-get -y install fontconfig
+sudo apt-get -y install fontconfig-config
+sudo apt-get -y install libfontconfig1-dev
+sudo apt-get -y install libxft-dev
+sudo apt-get -y install libcairo-dev
