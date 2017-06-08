d52a08ea7b3a076453ff175c64d36d818f51063b
lucidbeaming <zenphoto@gmail.com>
2017-06-08 02:53:19 -0700

    zynlaunch addition

---
+++ ZynAddSubFX/zynlaunch.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
+if pgrep jackd
+then
+echo Jacked up already
+else
+jackd -p16 -t2000 -d alsa -dhw:sndrpihifiberry -p 1024 -n 2 -r 48000 -s &
+sleep 1
+	if pgrep jackd
+	then
+	echo Jacked up
+	else
+	echo jackd failed for some reason
+	fi
+fi
+
+if pgrep zynaddsubfx
+then
+echo Zynaddsubfx is already singing
+else
+zynaddsubfx -U -A=0 -a -o 512 -r 48000 -b 512 -I alsa -O jack -P 7777 -L "/usr/local/share/zynaddsubfx/banks/Choir and Voice/0034-Slow Morph_Choir.xiz" &
+sleep 4
+	if pgrep zynaddsubfx
+	then
+	echo Zyn is singing
+	else
+	echo Zyn blorked. Epic Fail.
+	fi
+fi
+
+mini=$(aconnect -o | grep "MINILAB")
+mpk=$(aconnect -o | grep "MPKmini2")
+mio=$(aconnect -o | grep "mio")
+
+if [[ $mini ]]
+then
+aconnect 'Arturia MINILAB':0 'ZynAddSubFX':0
+echo Connected to MINIlab
+elif [[ $mpk ]]
+then
+aconnect 'MPKmini2':0 'ZynAddSubFX':0
+echo Connected to MPKmini
+elif [[ $mio ]]
+then
+aconnect 'mio':0 'ZynAddSubFX':0
+echo Connected to Mio
+else
+echo No known midi devices available. Try aconnect -l
+fi
+
+cd ZynAddSubFX-WebApp
+node index.js
+cd ..
+
+exit
+
